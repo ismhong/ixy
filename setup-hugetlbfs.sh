b2d6db7bfdbbae44e638b06140824119de4a4f4f
tsw303005 <tsw303005@gmail.com>
2025-04-20 23:34:19 +0800

    support hugetlbfs for pi5 The original implementation only supported modifying the hugepage count for a specific node on architectures with NUMA. The code has been updated to support configuring hugepages on the Raspberry Pi 5 as well.
    Signed-off-by: Ivan Ou <tsw303005@gmail.com>

--- setup-hugetlbfs.sh
+++ setup-hugetlbfs.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-mkdir -p /mnt/huge
-(mount | grep /mnt/huge) > /dev/null || mount -t hugetlbfs hugetlbfs /mnt/huge
-for i in /sys/devices/system/node/node[0-9]*
-do
-	echo 512 > "$i"/hugepages/hugepages-2048kB/nr_hugepages
-done
+
+if grep -q HugePages_Total /proc/meminfo; then
+	echo "[INFO] HugePage support detected, setting 1024 hugepages..."
+	echo '1024' | sudo tee -a /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+	mkdir -p /mnt/huge
+	(mount | grep /mnt/huge) > /dev/null || mount -t hugetlbfs hugetlbfs /mnt/huge
+	cat /proc/meminfo | grep -i huge
+else
+	echo "[ERROR] HugePage not supported on this system!" >&2
+fi
