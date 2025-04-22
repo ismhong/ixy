f54987b83e15c920db49e3dfa53adbe7b100e219
tsw303005 <tsw303005@gmail.com>
2025-04-23 00:15:35 +0800

    Update hugetlbfs scripts Before setting mount point, it should umount the mount point before. So, update hugetlbfs scripts to do it.
    Signed-off-by: Ivan Ou <tsw303005@gmail.com>

--- setup-hugetlbfs.sh
+++ setup-hugetlbfs.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+if mountpoint -q /mnt/huge; then
+	echo "Clear mount point first..."
+    umount /mnt/huge
+fi
+
 if grep -q HugePages_Total /proc/meminfo; then
-	echo "[INFO] HugePage support detected, setting 1024 hugepages..."
-	echo '1024' | sudo tee -a /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+	echo "[INFO] HugePage support detected, setting 512 hugepages..."
 	mkdir -p /mnt/huge
 	(mount | grep /mnt/huge) > /dev/null || mount -t hugetlbfs hugetlbfs /mnt/huge
+	echo '512' | sudo tee -a /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
 	cat /proc/meminfo | grep -i huge
 else
 	echo "[ERROR] HugePage not supported on this system!" >&2
